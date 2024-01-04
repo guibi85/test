fa96139d8c129a85ee31bb77c0100de7bd273cc4
guibi85 <154674764+guibi85@users.noreply.github.com>
2024-01-04 13:19:45 +0900

    test2

--- src/com/test/helloWorld.java
+++ src/com/test/helloWorld.java
@@ -3,6 +3,6 @@ package com.test;
 public class helloWorld {
 	
 	public final static void main(String args[]) {
-		System.out.println("hello world!!");
+		System.out.println("hello world2222222!!");
 	}
 }
